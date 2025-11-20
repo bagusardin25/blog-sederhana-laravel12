f1ddd701bc8238cadc57f114b5042fa69f20c8a1
Bagus Ardin Prayoga <bagusardinp@gmail.com>
2025-11-20 14:55:08 +0700

    add function in postcontroller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,5 +6,54 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    //Data dummy
+    private function getPosts(){
+        return [
+            [
+                'id' => 1,
+                'title' => 'Belajar laravel12',
+                'content' => 'Laravel adalah framework php yang popular.',
+                'author' => 'Admin',
+                'created_at' => '2025-20-11'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Mengeal Blade Templaet',
+                'content' => 'Blade adalah template engine bawaan laravel',
+                'author' => 'Admin',
+                'create_at' => '2025-20-11'
+            ]
+        ];
+    }
+
+    //Menampilkan semua post
+    public function index(){
+        $posts = $this->getPosts();
+        return view('posts.index', compact('posts'));
+    }
+
+    //Menampilkan form create
+    public function create(){
+        return view('posts.create');
+    }
+
+    //Menyimpan post baru (contoh validasi)
+    public function store(Request $request){
+        $request->validate([
+            'title' => 'required|max:225',
+            'content' => 'required',
+            'author' => 'required|max:100'
+        ]);
+
+        //lalu code dibawah ini untuk menyimpan ke databasenya
+        return redirect()->route('posts.index')
+        ->with('success', 'Post berhasil dibuat!');
+    }
+
+    //Menampilkan detail post
+    public function show($id){
+        $posts = $this->getPosts();
+        $post = collect($posts)->firstWhere('id', $id);
+        return view('posts.show', compact('post'));
+    }
 }
